2f113ba07c7cb94949b31aaee4c0f7d1ddf1a254
Yum-git <yzk.yuma@gmail.com>
2021-11-16 16:55:39 +0900

    Fix カレンダーに予定を追加できるよう変更しました

--- src/pages/routing/Calender.jsx
+++ src/pages/routing/Calender.jsx
@@ -1,35 +1,98 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
+import Grid from "@material-ui/core/Grid";
+import {EditingState, IntegratedEditing, ViewState} from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
-    DayView,
+    WeekView,
+    Toolbar,
+    DateNavigator,
     Appointments,
+    AppointmentForm,
+    AppointmentTooltip,
+    TodayButton,
+    ConfirmationDialog
 } from '@devexpress/dx-react-scheduler-material-ui';
+import {makeStyles} from "@material-ui/core/styles";
+import {useState} from "react";
 
-const currentDate = '2018-11-01';
-const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        textAlign: 'center',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        margin: theme.spacing(2),
+        textAlign: 'center',
+    }
+}));
+
+
+const now = new Date();
 
 
 const Calender = () => {
+    const classes = useStyles();
+
+    const [data, setData] = useState([
+        { startDate: '2010-01-01T09:45', endDate: '2010-01-01T11:00', title: 'Don`t Delete!!', id: 0 },
+    ]);
+
+    const [currentDate, setCurrentDate] = useState(now);
+
+    const commitChanges = ({added, changed, deleted}) => {
+        if(added){
+            setData([...data, { id: data[data.length - 1].id + 1, ...added }]);
+        }
+
+        if(changed){
+            setData(data.map(appointment => (changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+        }
+
+        if(deleted){
+            setData(data.filter(appointment => appointment.id !== deleted));
+        }
+    }
+
     return(
-        <Paper>
-            <Scheduler
-                data={schedulerData}
-            >
-                <ViewState
-                    currentDate={currentDate}
-                />
-                <DayView
-                    startDayHour={9}
-                    endDayHour={14}
-                />
-                <Appointments />
-            </Scheduler>
-        </Paper>
+        <div className="Graph-Container">
+            <div className={classes.root}>
+                <Grid container alignItems="center" justify="center" spacing={2}>
+                    <Grid item xs={12} className={classes.paper}>
+                        <Paper>
+                            <Scheduler
+                                data={data}
+                                height={660}
+                            >
+                                <ViewState
+                                    currentDate={currentDate}
+                                    onCurrentDateChange={setCurrentDate}
+                                />
+                                <EditingState
+                                    onCommitChanges={commitChanges}
+                                />
+                                <IntegratedEditing />
+                                <WeekView
+                                    startDayHour={9}
+                                    endDayHour={19}
+                                />
+                                <Toolbar />
+                                <DateNavigator />
+                                <TodayButton />
+                                <ConfirmationDialog />
+                                <Appointments />
+                                <AppointmentTooltip
+                                    showOpenButton
+                                    showDeleteButton
+                                />
+                                <AppointmentForm />
+                            </Scheduler>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
     )
 };
 
